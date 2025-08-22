9155b35375ab4b693f0cbfe8c2d194125c628db1
pmoukda <moukda_p@hotmail.com>
2025-08-22 07:55:30 -0400

    Ajouter un petite animation du carrousel lors du passage d'une image à l'autre

--- gabarit/carrousel.php
+++ gabarit/carrousel.php
@@ -4,28 +4,23 @@
  * Permet d'afficher le carrousel de la section héro
  */
 
-  $hero_background = [];
+$hero_background = [];
   
-  for ($i = 0; $i < 10; $i++) {
-      $image_url = get_theme_mod("hero_background_$i");
-      if ($image_url) {
-          $hero_background[] = $image_url;
-      }
+for ($i = 0; $i < 10; $i++) {
+    $image_url = get_theme_mod("hero_background_$i");
+    if ($image_url) {
+      $hero_background[] = $image_url;
+    }
   }
   ?>
-    <?php foreach ($hero_background as $index => $url): ?>
-      <div class="carrousel"
-           style="background-image: url('<?= esc_url($url) ?>'); opacity: <?= $index === 0 ? '1' : '0' ?>;">
-      </div>
-    <?php endforeach; ?>
+<?php foreach ($hero_background as $index => $url): ?>
+  <div class="carrousel"
+    style="background-image: url('<?= esc_url($url) ?>'); opacity: <?= $index === 0 ? '1' : '0' ?>;">
+  </div>
+<?php endforeach; ?>
 
-    <form class="carrousel__form">
-      <?php foreach ($hero_background as $index => $url): ?>
-        <input type="radio"
-               class="carrousel__radio"
-               name="carrousel__radio"
-               id="carrousel-radio-<?= $index ?>"
-               <?= $index === 0 ? 'checked' : '' ?>>
-      <?php endforeach; ?>
-    </form>
-?> 
+<form class="carrousel__form">
+  <?php foreach ($hero_background as $index => $url): ?>
+    <input type="radio" class="carrousel__radio" name="carrousel__radio" id="carrousel-radio<?= $index ?>" <?= $index === 0 ? 'checked' : '' ?>>
+  <?php endforeach; ?>
+</form> 
